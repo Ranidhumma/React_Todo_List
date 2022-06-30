3841d8625a1a99b929a13b97225d56b40ddd934c
Ranidhumma <ranidhumma8@gmail.com>
2022-07-01 01:10:21 +0530

    [Rani]Refactored code for app js and form Js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./component/Form"
 import FilterButton from "./component/FilterButton";
 import TodoList from './component/TodoList';
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map(task => (
     <TodoList
-        id={task.id}
-        name={task.name}
-        completed={task.completed}
-        key={task.id}
-      />
-    )
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  )
   );
-  return (
-    <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <Form />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        {taskList}
-      </ul>
+
+  function addTask(name) {
+    // alert(name);
+
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false }
+    setTasks([...tasks, newTask]);
+  }
+
+
+
+return (
+  <div className="todoapp stack-large">
+    <h1>TodoMatic</h1>
+    <Form addTask={addTask} />
+    <div className="filters btn-group stack-exception">
+      <FilterButton />
+      <FilterButton />
+      <FilterButton />
     </div>
-  );
+    <h2 id="list-heading">3 tasks remaining</h2>
+    <ul
+      role="list"
+      className="todo-list stack-large stack-exception"
+      aria-labelledby="list-heading"
+    >
+      {taskList}
+    </ul>
+  </div>
+);
+
 }
 
 
