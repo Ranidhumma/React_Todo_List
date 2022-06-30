4a1988ceb4d977b333bd48c32318401b8f51dc6f
Ranidhumma <ranidhumma8@gmail.com>
2022-07-01 01:20:19 +0530

    [Rani]Add. code for counting task

--- src/App.js
+++ src/App.js
@@ -18,34 +18,38 @@ function App(props) {
   )
   );
 
-  function addTask(name) {
-    // alert(name);
 
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  ;
+
+  function addTask(name) {
+    // alert(name); 
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false }
     setTasks([...tasks, newTask]);
   }
 
 
 
-return (
-  <div className="todoapp stack-large">
-    <h1>TodoMatic</h1>
-    <Form addTask={addTask} />
-    <div className="filters btn-group stack-exception">
-      <FilterButton />
-      <FilterButton />
-      <FilterButton />
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
-    <h2 id="list-heading">3 tasks remaining</h2>
-    <ul
-      role="list"
-      className="todo-list stack-large stack-exception"
-      aria-labelledby="list-heading"
-    >
-      {taskList}
-    </ul>
-  </div>
-);
+  );
 
 }
 
